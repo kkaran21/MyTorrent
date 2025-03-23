97fa7e3ed7470cdc6839fdd70e61a73c2027dba6
karan <khadkakaran95@gmail.com>
2025-03-23 22:05:26 +0530

    23 march

--- MyTorrentBackend/obj/Debug/net8.0/MyTorrentBackend.AssemblyInfo.cs
+++ MyTorrentBackend/obj/Debug/net8.0/MyTorrentBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyTorrentBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b01c3b78544fe93c5b0ec8746c13cf5320852c48")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e893650cd41393bb58de4ae7d30f64fb78d271c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyTorrentBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyTorrentBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
